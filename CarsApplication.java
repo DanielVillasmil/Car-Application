68b27e2b70d89cdd627bb23bea1d028242dd1206
Daniel Villasmil <dvillasmil08@gmail.com>
2020-01-15 16:00:50 -0500

    First commit

--- CarsApplication.java
+++ CarsApplication.java
@@ -3,13 +3,13 @@ package org.wcci;
 public class CarsApplication {
 
     public static void main(String[] args) {
-        Car car1 = new Car( "Snow Tire", "Blue", "Auto", new Engine("4-cl",4));
+        Car car1 = new Car("Snow Tire", "Blue", "Auto", new Engine("4-cl", 4));
         car1.accelerate();
         car1.drive();
         car1.stop();
         car1.fuelcar();
         System.out.println(car1.getColor());
-        Car car2 = new Car ( "Regular", "Green", "Auto", new Engine("V8", 10));
+        Car car2 = new Car("Regular", "Green", "Auto", new Engine("V8", 10));
         car1.accelerate();
         car1.drive();
         car1.stop();
