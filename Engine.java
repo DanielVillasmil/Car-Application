5c50eb874ee0bf80259922963a6fcf4f7c0e7d66
Daniel Villasmil <dvillasmil08@gmail.com>
2020-01-15 15:58:11 -0500

    Car APP first commit

---
+++ Engine.java
@@ -0,0 +1,15 @@
+package org.wcci;
+
+public class Engine {
+    private String engineType;
+    private int fuelBurnRate;
+
+    public Engine(String engineType, int fuelBurnRate) {
+        this.engineType = engineType;
+        this.fuelBurnRate = fuelBurnRate;
+    }
+
+    public int getFuelBurnRate() {
+        return fuelBurnRate;
+    }
+}
